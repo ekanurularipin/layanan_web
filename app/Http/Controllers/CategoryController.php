a2de4ae5b54817b37c281311435dc224137a2081
unknown <ekanurularipin@gmail.com>
2024-06-25 16:33:01 +0700

    category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+
+class CategoryController extends Controller
+{
+    public function __construct(){
+        $this ->middleware('auth:api',['except'=>'index']);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::all();
+
+        return Response()->json([
+            'data' => $categories
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $category = Category::create($request-> all());
+
+        return Response()->json([
+            'data' => $category
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        // return response()->json($request->all());
+        $category->update($request->all());
+
+        return Response() ->json([
+            'message' => 'berhasil',
+            'data' => $category
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return Response() ->json([
+            'message' => 'berhasil'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,12 @@ Route::group([
     'prefix' => 'auth'
 ], function(){
     Route::post('login', [AuthController::class, 'login'])->name('login');
+});
+
+Route::group([
+    'middleware' => 'api'
+], function(){
+    Route::resources([
+        'categories' => CategoryController::class
+    ]);
 });
